da14dcbbe36ca16dbd72845850f4f095e6b2be3d
4b-n4v <ab.navilon@gmail.com>
2025-02-25 21:57:59 -0600

    Fixed custom user model and migration conflicts

--- senior_project_app/settings.py
+++ senior_project_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'projects.apps.ProjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -99,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = 'projects.User'
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
