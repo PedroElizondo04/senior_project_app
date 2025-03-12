04e47dababea7695fafc2f565a8349b1b0bc7104
aBall360 <115499400+aBall360@users.noreply.github.com>
2025-03-12 08:23:00 +0000

    First versions of loginpage, landingpage, and projectproposalpage

--- senior_project_app/settings.py
+++ senior_project_app/settings.py
@@ -73,7 +73,6 @@ TEMPLATES = [
 STATIC_URL = "static/"
 WSGI_APPLICATION = 'senior_project_app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -130,6 +129,10 @@ USE_TZ = True
 # )
 #
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
